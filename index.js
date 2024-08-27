944a3d36f671bfb20c64deddbc464e7b23f12c62
Isaiah <mancha.isaiah@gmail.com>
2024-08-27 17:33:57 -0400

    flow lab

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(someNumber){
+  let result
+   if (someNumber <= 400) {
+    result = "This one is on me!"
+   } else if (someNumber > 400 && someNumber <= 2000){
+    result = "That will be twenty bucks."
+   } else if (someNumber > 2000 && someNumber <= 2500){
+    result = "I will gladly take your thirty bucks."
+   } else {
+    return 'No can do.';
+   }
+  return result;
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  function ternaryCheckCity(city) {
+    return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
